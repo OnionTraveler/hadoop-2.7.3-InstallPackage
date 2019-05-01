1689b887a236a5bd95bb5cfd5c3f0925e285f194
onionTraveler <onionTraveler@gmail.com>
2019-05-02 03:09:22 +0800

    spark-2.4.0-InstallPackage

---
+++ restartANDstop/spark-2.4.0-InstallPackage/spark-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+mkdir ../hadoop_StartStop ../spark_StartStop
+mv ../auto-restart.sh ../hadoop_StartStop
+mv ../auto-stop.sh ../hadoop_StartStop
+mv ../how2do.txt ../hadoop_StartStop
+echo '「source start-all.sh」【AFTER】 you start hadoop deamons in hadoop_StartStop directory' >> ../spark_StartStop/how2do4spark.txt
+echo '「source stop-all.sh」【BEFORE】 you stop hadoop deamons in hadoop_StartStop directory' >> ../spark_StartStop/how2do4spark.txt
+
+cd /tmp; wget https://archive.apache.org/dist/spark/spark-2.4.0/spark-2.4.0-bin-hadoop2.6.tgz && tar zxvf /tmp/spark-2.4.0-bin-hadoop2.6.tgz && rm /tmp/spark-2.4.0-bin-hadoop2.6.tgz
+cd /tmp; mv spark-2.4.0-bin-hadoop2.6 .spark-2.4.0-bin-hadoop2.6 && mv .spark-2.4.0-bin-hadoop2.6 ~
+ln -s ~/.spark-2.4.0-bin-hadoop2.6/sbin/start-all.sh ~/restartANDstop/spark_StartStop/start-all.sh
+ln -s ~/.spark-2.4.0-bin-hadoop2.6/sbin/stop-all.sh ~/restartANDstop/spark_StartStop/stop-all.sh
+
+echo 'export SPARK_HOME=~/.spark-2.4.0-bin-hadoop2.6' >> ~/.bashrc
+echo 'export PATH=$SPARK_HOME/bin:$PATH' >> ~/.bashrc
+echo 'export PYSPARK_PYTHON=python3' >> ~/.bashrc
+echo 'export PYSPARK_DRIVER_PYTHON=ipython3' >> ~/.bashrc
+source ~/.bashrc
+
+rm -r -f ../spark-2.4.0-InstallPackage
