08526cbc565489005fcd7ae1d24d88d228d6c70f
onionTraveler <onionTraveler@gmail.com>
2019-05-03 17:55:01 +0800

    Hadoop Command Operation

--- restartANDstop/spark-2.4.0-InstallPackage/spark-install.sh
+++ restartANDstop/spark-2.4.0-InstallPackage/spark-install.sh
@@ -3,6 +3,7 @@ mkdir ../hadoop_StartStop ../spark_StartStop
 mv ../auto-restart.sh ../hadoop_StartStop
 mv ../auto-stop.sh ../hadoop_StartStop
 mv ../how2do.txt ../hadoop_StartStop
+mv ../Hadoop_Command_Operation.txt ../hadoop_StartStop 
 echo '「source start-all.sh」【PAFTER】 you start hadoop deamons in hadoop_StartStop directory' >> ../spark_StartStop/how2do4spark.txt
 echo '「source stop-all.sh」【BEFORE】 you stop hadoop deamons in hadoop_StartStop directory' >> ../spark_StartStop/how2do4spark.txt
 
