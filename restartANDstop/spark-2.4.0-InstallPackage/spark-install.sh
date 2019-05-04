d161d1ae7b3dc0a8826b162abb4038aa466b810c
OnionTraveler <47844836+OnionTraveler@users.noreply.github.com>
2019-05-05 00:13:27 +0800

    Update spark-install.sh

--- restartANDstop/spark-2.4.0-InstallPackage/spark-install.sh
+++ restartANDstop/spark-2.4.0-InstallPackage/spark-install.sh
@@ -8,22 +8,22 @@ echo '「source start-all.sh」【AFTER】 you start hadoop deamons in hadoop_St
 echo '「source stop-all.sh」【BEFORE】 you stop hadoop deamons in hadoop_StartStop directory' >> ../spark_StartStop/how2do4spark.txt
 
 
-cd /tmp; wget https://archive.apache.org/dist/spark/spark-2.4.0/spark-2.4.0-bin-hadoop2.7.tgz && tar zxvf /tmp/spark-2.4.0-bin-hadoop2.7.tgz && rm /tmp/spark-2.4.0-bin-hadoop2.7.tgz
-cd /tmp; mv spark-2.4.0-bin-hadoop2.7 .spark-2.4.0-bin-hadoop2.7 && mv .spark-2.4.0-bin-hadoop2.7 ~
+cd /tmp; wget https://archive.apache.org/dist/spark/spark-2.4.2/spark-2.4.2-bin-hadoop2.7.tgz && tar zxvf /tmp/spark-2.4.2-bin-hadoop2.7.tgz && rm /tmp/spark-2.4.2-bin-hadoop2.7.tgz
+cd /tmp; mv spark-2.4.2-bin-hadoop2.7 .spark-2.4.2-bin-hadoop2.7 && mv .spark-2.4.2-bin-hadoop2.7 ~
 
 
 cat >> ~/restartANDstop/spark_StartStop/stop-all4Spark.sh << ONION
 source /etc/profile
-source ~/.spark-2.4.0-bin-hadoop2.7/sbin/stop-all.sh
+source ~/.spark-2.4.2-bin-hadoop2.7/sbin/stop-all.sh
 ONION
 
 cat >> ~/restartANDstop/spark_StartStop/start-all4Spark.sh << ONION
 source /etc/profile
-source ~/.spark-2.4.0-bin-hadoop2.7/sbin/start-all.sh
+source ~/.spark-2.4.2-bin-hadoop2.7/sbin/start-all.sh
 ONION
 
 
-echo 'export SPARK_HOME=~/.spark-2.4.0-bin-hadoop2.7' >> ~/.bashrc
+echo 'export SPARK_HOME=~/.spark-2.4.2-bin-hadoop2.7' >> ~/.bashrc
 echo 'export PATH=$SPARK_HOME/bin:$PATH' >> ~/.bashrc
 echo 'export PYSPARK_PYTHON=python3' >> ~/.bashrc
 echo 'export PYSPARK_DRIVER_PYTHON=ipython3' >> ~/.bashrc
@@ -34,5 +34,5 @@ source /etc/profile
 source ~/restartANDstop/spark_StartStop/stop-all4Spark.sh
 source ~/restartANDstop/spark_StartStop/start-all4Spark.sh
 
-cd; rm -r -f ~/restartANDstop/spark-2.4.0-InstallPackage
+cd; rm -r -f ~/restartANDstop/spark-2.4.2-InstallPackage
 
