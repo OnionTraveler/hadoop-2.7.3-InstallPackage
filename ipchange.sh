00dc637cb4a01f26ba861cd4d8b6c358279bec26
OnionTraveler <47844836+OnionTraveler@users.noreply.github.com>
2019-05-04 13:33:40 +0800

    Update ipchange.sh

--- ipchange.sh
+++ ipchange.sh
@@ -1,25 +1,24 @@
 #You STILL CHECK whether IPs are correct again before you source settingup.sh
 
 #!/bin/bash
-ipmaster=xxx.xxx.xxx.xxx  # 192.168.233.153 # 172.17.0.2
-ipslaver1=xxx.xxx.xxx.xxx # 192.168.233.151 # 172.17.0.3
-ipslaver2=xxx.xxx.xxx.xxx # 192.168.233.152 # 172.17.0.4
+ipmaster=XXX.XXX.XXX.XXX  # XXX.XXX.XXX.XXX -> # 192.168.233.153 # 172.17.0.2
+ipslaver1=XXX.XXX.XXX.XXX # XXX.XXX.XXX.XXX -> # 192.168.233.151 # 172.17.0.3
+ipslaver2=XXX.XXX.XXX.XXX # XXX.XXX.XXX.XXX -> # 192.168.233.152 # 172.17.0.4
 
 
 #=========================0 (for settingup.sh)
-sed -i "s/ipmaster=xxx.xxx.xxx.xxx/ipmaster=$ipmaster/g" /tmp/hadoop-2.7.3-InstallPackage/settingup.sh
-sed -i "s/ipslaver1=xxx.xxx.xxx.xxx/ipslaver1=$ipslaver1/g" /tmp/hadoop-2.7.3-InstallPackage/settingup.sh
-sed -i "s/ipslaver2=xxx.xxx.xxx.xxx/ipslaver2=$ipslaver2/g" /tmp/hadoop-2.7.3-InstallPackage/settingup.sh
+sed -i "s/ipmaster=xxx.xxx.xxx.xxx/ipmaster=$ipmaster/g" ./settingup.sh
+sed -i "s/ipslaver1=xxx.xxx.xxx.xxx/ipslaver1=$ipslaver1/g" ./settingup.sh
+sed -i "s/ipslaver2=xxx.xxx.xxx.xxx/ipslaver2=$ipslaver2/g" ./settingup.sh
 
 
 #=========================1 (for auto-restart.sh)
-sed -i "s/ipmaster=xxx.xxx.xxx.xxx/ipmaster=$ipmaster/g" /tmp/hadoop-2.7.3-InstallPackage/restartANDstop/auto-restart.sh
-sed -i "s/ipslaver1=xxx.xxx.xxx.xxx/ipslaver1=$ipslaver1/g" /tmp/hadoop-2.7.3-InstallPackage/restartANDstop/auto-restart.sh
-sed -i "s/ipslaver2=xxx.xxx.xxx.xxx/ipslaver2=$ipslaver2/g" /tmp/hadoop-2.7.3-InstallPackage/restartANDstop/auto-restart.sh
+sed -i "s/ipmaster=xxx.xxx.xxx.xxx/ipmaster=$ipmaster/g" ./restartANDstop/auto-restart.sh
+sed -i "s/ipslaver1=xxx.xxx.xxx.xxx/ipslaver1=$ipslaver1/g" ./restartANDstop/auto-restart.sh
+sed -i "s/ipslaver2=xxx.xxx.xxx.xxx/ipslaver2=$ipslaver2/g" ./restartANDstop/auto-restart.sh
 
 
 #=========================2 (for auto-stop.sh)
-sed -i "s/ipmaster=xxx.xxx.xxx.xxx/ipmaster=$ipmaster/g" /tmp/hadoop-2.7.3-InstallPackage/restartANDstop/auto-stop.sh
-sed -i "s/ipslaver1=xxx.xxx.xxx.xxx/ipslaver1=$ipslaver1/g" /tmp/hadoop-2.7.3-InstallPackage/restartANDstop/auto-stop.sh
-sed -i "s/ipslaver2=xxx.xxx.xxx.xxx/ipslaver2=$ipslaver2/g" /tmp/hadoop-2.7.3-InstallPackage/restartANDstop/auto-stop.sh
-
+sed -i "s/ipmaster=xxx.xxx.xxx.xxx/ipmaster=$ipmaster/g" ./restartANDstop/auto-stop.sh
+sed -i "s/ipslaver1=xxx.xxx.xxx.xxx/ipslaver1=$ipslaver1/g" ./restartANDstop/auto-stop.sh
+sed -i "s/ipslaver2=xxx.xxx.xxx.xxx/ipslaver2=$ipslaver2/g" ./restartANDstop/auto-stop.sh
