e2bda8b2c29ddc1badbdf4c36e2527de22290dcc
OnionTraveler <47844836+OnionTraveler@users.noreply.github.com>
2019-05-05 20:43:44 +0800

    Update spark-install.sh
    Scala and Spark installation

--- restartANDstop/spark-2.4.2-InstallPackage/spark-install.sh
+++ restartANDstop/spark-2.4.2-InstallPackage/spark-install.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
+
+#========================= (主節點與各節點IP設定) =========================#
 ipmaster=xxx.xxx.xxx.xxx  # 192.168.233.153 # 172.17.0.2
 ipslaver1=xxx.xxx.xxx.xxx # 192.168.233.151 # 172.17.0.3
 ipslaver2=xxx.xxx.xxx.xxx # 192.168.233.152 # 172.17.0.4
 
 
-#========================= (for scala2.12.8)
+#========================= (for scala2.12.8) =========================#
+#spark是用scala寫的，故安裝執行啟動spark前，scala預先安裝在各節點且可執行是必要的！
+#========================= (下載scala-2.12.8執行檔)
 source /etc/profile
-cd /tmp; wget https://downloads.lightbend.com/scala/2.12.8/scala-2.12.8.tgz && tar zxvf /tmp/scala-2.12.8.tgz
+cd /tmp; wget https://downloads.lightbend.com/scala/2.12.8/scala-2.12.8.tgz; tar zxvf /tmp/scala-2.12.8.tgz
 cd /tmp; mkdir /usr/scala && mv scala-2.12.8 /usr/scala && ln -s /usr/scala/scala-2.12.8 /usr/scala/scala
-ln -s /usr/java/jdk1.8.0_144/ /usr/java/java
+
+
+#========================= (基本環境變數設定)
 echo 'export SCALA_HOME=/usr/scala/scala' >> /etc/profile
 echo 'export PATH=$SCALA_HOME/bin:$PATH' >> /etc/profile
 source /etc/profile
 
+
+#========================= (各節點scala的執行檔與基本環境變數設定)
 scp -rp /tmp/scala-2.12.8.tgz  $ipslaver1:/tmp/scala-2.12.8.tgz 
 scp -rp /tmp/scala-2.12.8.tgz  $ipslaver2:/tmp/scala-2.12.8.tgz 
 
@@ -33,45 +41,51 @@ ln -s /usr/java/jdk1.8.0_144/ /usr/java/java
 source /etc/profile
 ONION
 
+
+#========================= (siao sheng miè ji)
 rm -f /tmp/scala-2.12.8.tgz
 ssh $ipslaver1 'rm -f /tmp/scala-2.12.8.tgz'
 ssh $ipslaver2 'rm -f /tmp/scala-2.12.8.tgz'
 
 
-#========================= (for spark-2.4.2-bin-hadoop2.7)
-cd ~/restartANDstop
-mkdir hadoop_StartStop spark_StartStop
-mv auto-restart.sh hadoop_StartStop
-mv auto-stop.sh hadoop_StartStop
-mv how2do.txt hadoop_StartStop
-mv Hadoop_Command_Operation.txt hadoop_StartStop 
-echo '「source start-all.sh」【AFTER】 you start hadoop deamons in hadoop_StartStop directory' >> spark_StartStop/how2do4spark.txt
-echo '「source stop-all.sh」【BEFORE】 you stop hadoop deamons in hadoop_StartStop directory' >> spark_StartStop/how2do4spark.txt
 
 
-cd /tmp; wget https://archive.apache.org/dist/spark/spark-2.4.2/spark-2.4.2-bin-hadoop2.7.tgz && tar zxvf /tmp/spark-2.4.2-bin-hadoop2.7.tgz && rm /tmp/spark-2.4.2-bin-hadoop2.7.tgz
-cd /tmp; mv spark-2.4.2-bin-hadoop2.7 .spark-2.4.2-bin-hadoop2.7 && mv .spark-2.4.2-bin-hadoop2.7 ~
 
+#========================= (for spark-2.4.2-bin-hadoop2.7) =========================#
+mkdir ../hadoop_StartStop ../spark_StartStop
+mv ../auto-restart.sh ../hadoop_StartStop
+mv ../auto-stop.sh ../hadoop_StartStop
+mv ../how2do.txt ../hadoop_StartStop
+mv ../Hadoop_Command_Operation.txt ../hadoop_StartStop 
+echo '「source start-all.sh」【AFTER】 you start hadoop deamons in hadoop_StartStop directory' >> ../spark_StartStop/how2do4spark.txt
+echo '「source stop-all.sh」【BEFORE】 you stop hadoop deamons in hadoop_StartStop directory' >> ../spark_StartStop/how2do4spark.txt
+
+
+#========================= (下載spark-2.4.2-bin-hadoop2.7執行檔)
+cd /tmp; [ -e spark-2.4.2-bin-hadoop2.7.tgz ] && echo "The 「spark-2.4.2-bin-hadoop2.7」 file has existed" || wget https://archive.apache.org/dist/spark/spark-2.4.2/spark-2.4.2-bin-hadoop2.7.tgz
+cd /tmp; tar zxvf /tmp/spark-2.4.2-bin-hadoop2.7.tgz && rm /tmp/spark-2.4.2-bin-hadoop2.7.tgz; mv spark-2.4.2-bin-hadoop2.7 /opt
 
 cat >> ~/restartANDstop/spark_StartStop/stop-all4Spark.sh << ONION
 source /etc/profile
-source ~/.spark-2.4.2-bin-hadoop2.7/sbin/stop-all.sh
+source /opt/spark-2.4.2-bin-hadoop2.7/sbin/stop-all.sh
 ONION
 
 cat >> ~/restartANDstop/spark_StartStop/start-all4Spark.sh << ONION
 source /etc/profile
-source ~/.spark-2.4.2-bin-hadoop2.7/sbin/start-all.sh
+source /opt/spark-2.4.2-bin-hadoop2.7/sbin/start-all.sh
 ONION
 
 
-echo 'export SPARK_HOME=~/.spark-2.4.2-bin-hadoop2.7' >> ~/.bashrc
+#========================= (基本環境變數設定)
+echo 'export SPARK_HOME=/opt/spark-2.4.2-bin-hadoop2.7' >> ~/.bashrc
 echo 'export PATH=$SPARK_HOME/bin:$PATH' >> ~/.bashrc
 echo 'export PYSPARK_PYTHON=python3' >> ~/.bashrc
 echo 'export PYSPARK_DRIVER_PYTHON=ipython3' >> ~/.bashrc
 
 
-cp ~/.spark-2.4.2-bin-hadoop2.7/conf/spark-env.sh.template ~/.spark-2.4.2-bin-hadoop2.7/conf/spark-env.sh 
-cat >> ~/.spark-2.4.2-bin-hadoop2.7/conf/spark-env.sh  << ONION
+#========================= (spark啟動時(start-all.sh)所讀取的環境變數設定)
+cp /opt/spark-2.4.2-bin-hadoop2.7/conf/spark-env.sh.template /opt/spark-2.4.2-bin-hadoop2.7/conf/spark-env.sh 
+cat >> /opt/spark-2.4.2-bin-hadoop2.7/conf/spark-env.sh  << ONION
 export SCALA_HOME=/usr/scala/scala
 export JAVA_HOME=/usr/java/java
 export HADOOP_HOME=/opt/hadoop/
@@ -81,14 +95,27 @@ SPARK_LOCAL_DIRS=~/.spark-2.4.2-bin-hadoop2.7
 SPARK_DRIVER_MEMORY=1G
 ONION
 
-scp -r ~/.spark-2.4.2-bin-hadoop2.7 spark@$ipslave1:~
-scp -r ~/.spark-2.4.2-bin-hadoop2.7 spark@$ipslave2:~
+
+#========================= (spark啟動時(start-all.sh)所讀取節點IP的位置設定)
+cp /opt/spark-2.4.2-bin-hadoop2.7/conf/slaves.template /opt/spark-2.4.2-bin-hadoop2.7/conf/slaves
+cat >> /opt/spark-2.4.2-bin-hadoop2.7/conf/spark-env.sh  << ONION
+$ipslave1
+$ipslave2
+ONION
 
 
+#========================= (spark啟動時(start-all.sh)各節點當然必須亦有在相同位置的spark執行檔與其相同的變數設定)
+scp -r /opt/spark-2.4.2-bin-hadoop2.7 root@$ipslave1:/opt/spark-2.4.2-bin-hadoop2.7
+scp -r /opt/spark-2.4.2-bin-hadoop2.7 root@$ipslave2:/opt/spark-2.4.2-bin-hadoop2.7
 
+
+#========================= (啟動Spark資源管理器(Spark Standalone))
 source ~/.bashrc
 source /etc/profile
 source ~/restartANDstop/spark_StartStop/stop-all4Spark.sh
 source ~/restartANDstop/spark_StartStop/start-all4Spark.sh
+
+
+#========================= (siao sheng miè ji)
 cd; rm -r -f ~/restartANDstop/spark-2.4.2-InstallPackage
 
