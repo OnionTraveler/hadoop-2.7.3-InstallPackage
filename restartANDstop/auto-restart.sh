0c0970463951f0ad94be846fbb410aab7f3dc505
OnionTraveler <47844836+OnionTraveler@users.noreply.github.com>
2019-05-05 21:43:23 +0800

    Update auto-restart.sh

--- restartANDstop/auto-restart.sh
+++ restartANDstop/auto-restart.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
+#========================= (主節點與各節點IP設定) =========================#
 ipmaster=xxx.xxx.xxx.xxx  # 192.168.233.153 # 172.17.0.2
 ipslaver1=xxx.xxx.xxx.xxx # 192.168.233.151 # 172.17.0.3
 ipslaver2=xxx.xxx.xxx.xxx # 192.168.233.152 # 172.17.0.4
-#=========================
+
+
+#========================= (啟動各節點的zookeeper)
 source /etc/profile
 /opt/zookeeper/bin/zkServer.sh start
 sleep 3
@@ -20,7 +24,7 @@ sleep 3
 ONION
 
 
-#=========================
+#========================= (啟動各節點的journalnode)
 source /etc/profile
 hadoop-daemon.sh start journalnode
 sleep 3
@@ -38,7 +42,8 @@ sleep 3
 ONION
 
 
-#=========================
+#========================= (啟動hadoop所需使用的所有服務精靈(start-all.sh))
 sleep 5
 source /etc/profile
 start-all.sh
+
